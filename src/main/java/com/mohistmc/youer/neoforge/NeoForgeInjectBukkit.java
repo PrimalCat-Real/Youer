c249ff570f9f43ed0dc6f06a533fa4c563721f7d
PrimalCat-Real <romanenko.denis2003@gmail.com>
2026-04-24 11:55:29 +0200

    fix

--- src/main/java/com/mohistmc/youer/neoforge/NeoForgeInjectBukkit.java
+++ src/main/java/com/mohistmc/youer/neoforge/NeoForgeInjectBukkit.java
@@ -77,7 +77,6 @@ public class NeoForgeInjectBukkit {
         addEnumMaterialInItems();
         addEnumEffectAndPotion();
         addEnumMaterialsInBlocks();
-        // addEnumMaterialsInBlockEntityType();
         addEnumBiome();
         addFluid();
         addEnumEntity();
@@ -137,17 +136,6 @@ public class NeoForgeInjectBukkit {
                     CraftMagicNumbers.MATERIAL_BLOCK.put(material, block);
                     debug("Save-BLOCK:{} - {}", material.name(), material.key);
                 }
-            }
-        }
-    }
-
-    public static void addEnumMaterialsInBlockEntityType() {
-        var registry = BuiltInRegistries.BLOCK_ENTITY_TYPE;
-        for (BlockEntityType<?> entityType : registry) {
-            ResourceLocation resourceLocation = registry.getKey(entityType);
-            if (isMods(resourceLocation)) {
-                String materialName = MohistDynamEnum.normalizeName(resourceLocation.toString());
-                Youer.LOGGER.error("Discover entity blocks:{} - {}", entityType, materialName);
             }
         }
     }
